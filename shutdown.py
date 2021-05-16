4ca00792f4d6248670d20e2a15658c69e8d87144
Shashwat-creator <69109132+Shashwat-creator@users.noreply.github.com>
2021-05-16 14:58:16 +0530

    Delete shutdown.py

--- shutdown.py
+++
@@ -1,6 +0,0 @@
-import socket
-socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-socket.connect(("localhost",5500))
-request="xipo8770"
-socket.sendall(bytes(str(len(request)).ljust(1024),encoding="utf-8"))
-socket.sendall(bytes(request,encoding="utf-8"))
